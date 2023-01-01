c6490199f254bbeb8b849abe2db5e391c82028aa
yiye-yfs <2148133527@qq.com>
2023-01-01 22:44:26 +0800

    update xor description

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
 from qsnctf import *
 
 
-a = base100_encode("qsnctf")
-print(a)
+a="adb"
+b="123"
+tmp=xor_list(a,b)
+print(tmp)
+c=[]
+for i in range(3):
+    c.append(chr(i))
+print(c)
+tmp=xor_list(a,c)
+print(tmp)
