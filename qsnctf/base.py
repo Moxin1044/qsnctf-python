a08dd892f4d98687c853297447e4dd8549921a52
末心 <59173630+Moxin1044@users.noreply.github.com>
2023-01-01 21:36:36 +0800

    add Base36

--- qsnctf/base.py
+++ qsnctf/base.py
@@ -118,30 +118,20 @@ def base32_encode(text):
 
 
 def base36_encode(number):
-    # 定义编码字符集
     charset = "0123456789abcdefghijklmnopqrstuvwxyz"
-    # 初始化编码后的字符串
     encoded = ""
-    # 当数字大于 0 时循环
     while number > 0:
-        # 取模计算当前位编码字符
         encoded = charset[number % 36] + encoded
-        # 除以 36 计算下一位
         number //= 36
-    # 如果编码后的字符串为空，则补 0
     if encoded == "":
         encoded = "0"
     return encoded
 
 
 def base36_decode(encoded):
-    # 定义编码字符集
     charset = "0123456789abcdefghijklmnopqrstuvwxyz"
-    # 初始化解码后的数字
     number = 0
-    # 遍历编码字符串，解码每一位
     for i, ch in enumerate(encoded[::-1]):
-        # 计算当前位的数字值
         number += charset.index(ch) * (36 ** i)
     return number
 
