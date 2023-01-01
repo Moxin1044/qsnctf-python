4b66406c2b8a149bb6a8f28af74ed5645cf699dc
末心 <59173630+Moxin1044@users.noreply.github.com>
2023-01-01 13:42:41 +0800

    修复了凯撒加密解密脚本的BUG

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import qsnctf
 import os
 
-
-a = qsnctf.base.base64_encode("青少年CTF")
+a = qsnctf.caesar_encrypt("qsnctf", 3)
 print(a)
+a = qsnctf.caesar_decrypt("tvqfwi", 3)
+print(a)
+
+a = qsnctf.crypto.caesar_decrypt_cracking("tvqfwi")
+for i in a:
+    print(i)
