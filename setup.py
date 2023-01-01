64daa6d9aa307cc73301f3ed8a2c7fb74c74bb6b
末心 <59173630+Moxin1044@users.noreply.github.com>
2023-01-01 18:34:54 +0800

    add base62

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with io.open('README.md', encoding='utf-8') as f:
 setup(
     name='qsnctf',
     version='0.0.5',
-    install_requires=['PyExecJS','python3base92'],
+    install_requires=['PyExecJS', 'python3base92', 'pybase62'],
     packages=find_packages(),
     include_package_data=True,
     package_data={'qsnctf': ['plugin/*']},
