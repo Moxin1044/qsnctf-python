c72668ed32abcd84850191a842b0baee1063b5e4
末心 <59173630+Moxin1044@users.noreply.github.com>
2023-01-01 14:06:10 +0800

    新增了部分HASH算法

--- test.py
+++ test.py
@@ -1,11 +1,6 @@
 import qsnctf
 import os
 
-a = qsnctf.caesar_encrypt("qsnctf", 3)
-print(a)
-a = qsnctf.caesar_decrypt("tvqfwi", 3)
-print(a)
 
-a = qsnctf.crypto.caesar_decrypt_cracking("tvqfwi")
-for i in a:
-    print(i)
+a = qsnctf.hash.shake_128("123","456")
+print(a)
