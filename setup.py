5d926e48ca980e01c69897ed4e971b65cc32f27a
末心 <59173630+Moxin1044@users.noreply.github.com>
2023-01-01 10:15:45 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import io
+
+with io.open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='qsnctf',
+    version='0.0.2',
+    packages=['qsnctf'],
+    url='https://github.com/Moxin1044/qsnctf-python',
+    license='MIT License',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Moxin',
+    author_email='1044631097@qq.com',
+    description='青少年CTF训练平台提供的Python软件包'
+)
