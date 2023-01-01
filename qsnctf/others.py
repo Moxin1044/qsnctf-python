ed0a660c3fa1e22b1f35e0c7c797d144cfa5f785
末心 <59173630+Moxin1044@users.noreply.github.com>
2023-01-01 10:38:26 +0800

    更新 社会主义核心价值观编码解码功能

--- qsnctf/others.py
+++ qsnctf/others.py
@@ -1,10 +1,12 @@
 # 其他CTF能力
 # 2023年1月1日
 # 末心
-
+import os
 import execjs  # PyExecJS
 # 社会主义核心价值观编码
 # 需要将Python目录下>lib>subprocess.py的765行附近的encoding的默认None值修改为utf-8即可。
+
+
 
 
 def js_from_file(file_name):
@@ -14,13 +16,15 @@ def js_from_file(file_name):
 
 
 def Chinese_socialism_encode(data):
-    content = execjs.compile(js_from_file('qsnctf/plugin/js/cvencode.js'))
+    folder_path = os.path.abspath(os.path.dirname(__file__))
+    content = execjs.compile(js_from_file(folder_path+'\\plugin\\js\\cvencode.js'))
     result = content.call('encode', data)
     result = str(result)
     return result
 
 def Chinese_socialism_decode(data):
-    content = execjs.compile(js_from_file('qsnctf/plugin/js/cvencode.js'))
+    folder_path = os.path.abspath(os.path.dirname(__file__))
+    content = execjs.compile(js_from_file(folder_path+'\\plugin\\js\\cvencode.js'))
     result = content.call('decode', data)
     result = str(result)
     return result
--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 import qsnctf
+import os
+
 
 a = qsnctf.others.Chinese_socialism_encode("社")
 print(a)
+folder_path = os.path.abspath(os.path.dirname(__file__))
+print(folder_path)
