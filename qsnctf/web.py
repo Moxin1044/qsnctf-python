22f1025ea5d7b582df6ff218a8924ad439849127
末心 <59173630+Moxin1044@users.noreply.github.com>
2023-01-04 16:08:47 +0800

    Dirscan

--- qsnctf/web.py
+++ qsnctf/web.py
@@ -1,14 +1,29 @@
 # web操作
 import requests
+import os
+from qsnctf.auxiliary import read_file_to_list,is_http_or_https_url,normalize_url
 
 
-class dirscan:
+class DirScan:
     def __init__(self, url, threadline=10, dirlist=None):
+        """
+        :param url: Sans URL
+        :param threadline: Thread line
+        :param dirlist: dirs list
+        """
         self.url = url
         self.threadline = threadline
-        if dirlist:
-            self.dirlist = dirlist
-            # TODO: dirlist in self, will san dirs
-        else:
-            # TODO: read local dirs
+        self.results = []
+        self.dirlist = dirlist
+
+    def scan(self):
+        if self.dirlist:
             pass
+        else:
+            package_path = os.path.abspath(os.path.dirname(__file__))
+            file_path = os.path.join(package_path, 'plugin', 'txt', 'dirs.txt')
+            self.dirlist = read_file_to_list(file_path)
+        if is_http_or_https_url(self.url):
+            pass
+        else:
+            raise ValueError("Invalid url")
--- test.py
+++ test.py
@@ -1,2 +1,3 @@
 from qsnctf import *
 
+DirScan('http://www.qsnctf.com')
