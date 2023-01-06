2795c9a43922f511bc0e710fb46b7dac47321a92
末心 <59173630+Moxin1044@users.noreply.github.com>
2023-01-06 13:49:41 +0800

    GUI Modes

--- GUI/main.py
+++ GUI/main.py
@@ -1,13 +1,27 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QDialog, QLineEdit, QLabel, QVBoxLayout
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_Main(object):
-    def setupUi(self, Main):
-        Main.setObjectName("MainWindow")
-        Main.resize(948, 745)
-        Main.setMaximumSize(QtCore.QSize(16777215, 16777215))
+class Dialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.init_UI()
+
+    def init_UI(self):
+        vbox = QVBoxLayout()
+        self.setWindowFlags(QtCore.Qt.WindowCloseButtonHint)  # Hide the "?" button
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap("favicon.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        Main.setWindowIcon(icon)
-        Main.setStyleSheet("")
-        Main.setIconSize(QtCore.QSize(30, 30))
+        self.setWindowIcon(icon)
+        self.resize(960, 750)
+        # self.hint = QLabel("Enter your name:")
+        # vbox.addWidget(self.hint)
+        self.setLayout(vbox)
+        self.setWindowTitle("青少年CTF-CTF工具 Developer 0.0.1")
+        self.show()
+
+
+app = QApplication(sys.argv)
+dialog = Dialog()
+sys.exit(app.exec_())
