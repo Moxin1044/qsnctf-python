b83deddb44b0658111b332895d201f4cdfd76576
末心 <1044631097@qq.com>
2023-02-20 06:03:21 +0800

    rarcrack

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with io.open('README.md', encoding='utf-8') as f:
 setup(
     name='qsnctf',
     version='0.0.8.8',
-    install_requires=['PyExecJS', 'pybase62', 'base58', 'requests', 'bs4', 'urllib3'],
+    install_requires=['PyExecJS', 'pybase62', 'base58', 'requests', 'bs4', 'urllib3', 'rarfile'],
     packages=find_packages(),
     include_package_data=True,
     package_data={'qsnctf': ['plugin/*']},
